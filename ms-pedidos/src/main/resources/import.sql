e9349b3bc8f8dfd7d7c43609f9c7d6003e8903fc
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-25 10:33:53 -0300

    feat: add import.sql

--- ms-pedidos/src/main/resources/import.sql
+++ ms-pedidos/src/main/resources/import.sql
@@ -1,5 +1,7 @@
-INSERT INTO tb_pedido(name, cpf, data, status, valorTotal) values ('João Veiera', 12345678910, '2025-11-25','ENTREGUE', 540);
-INSERT INTO tb_pedido(name, cpf, data, status, valorTotal) values ('Kézia', 12345678910, '2026-11-30','ENTREGUE', 80);
+INSERT INTO tb_pedido(name, cpf, data, status, valor_total) values ('João Veiera', 12345678910, '2025-11-25','CRIADO', 540.0);
+INSERT INTO tb_pedido(name, cpf, data, status, valor_total) values ('Kézia', 12345678910, '2026-11-30','CRIADO', 359.0);
 
 
-INSERT INTO tb_item_do_pedido(preco_unitario, pedido_id, descricao) values (250, 2, 1, "Mouse ");
+INSERT INTO tb_item_do_pedido(quantidade, descricao, preco_unitario, pedido_id) values ( 2, "Mouse sem fio Microsoft", 250.0, 1);
+INSERT INTO tb_item_do_pedido(quantidade, descricao, preco_unitario, pedido_id) values ( 1, "Tecalado sem fio Microsoft", 290.0, 1);
+INSERT INTO tb_item_do_pedido(quantidade, descricao, preco_unitario, pedido_id) values ( 1, "Smart TV LG LED", 3599.0, 2);
