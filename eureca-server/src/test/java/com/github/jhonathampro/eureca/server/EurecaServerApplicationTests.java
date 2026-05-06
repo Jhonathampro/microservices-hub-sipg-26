1099db0989f74d68bfd72c86356d197c19c64c58
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-06 11:38:30 -0300

    chore: add Eureka-server, congig Ms-pedidos client eureka

---
+++ eureca-server/src/test/java/com/github/jhonathampro/eureca/server/EurecaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jhonathampro.eureca.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurecaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
